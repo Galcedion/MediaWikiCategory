fefb999514e3c65a6cdd1f2a00a9db1127661194
Galcedion <galcedion@gmx.de>
2026-02-18 21:20:15 +0100

    preparing new bg control system restructuring category scape process

--- background.js
+++ background.js
@@ -5,6 +5,8 @@ var targetHref;
 var caller = false;
 var categeoriesOnPage;
 var tmpStorage = {};
+var lock = false;
+var categoryToDo = [];
 
 // defining menu and browser action
 browser.menus.create({
@@ -60,47 +62,63 @@ function storeFromPopup(listener) {
 }
 
 // main storage function, creating a promise
-function storageManager(caller = false) {
+function storageManager(caller = false, rerun = false) {
+	let metadata = {'caller': caller}
+	if(!lock) {
+		lock = true;
+		metadata['targetTitle'] = targetTitle;
+		metadata['targetHref'] = targetHref;
+	} else if(rerun) {
+		metadata['targetTitle'] = categoryToDo[0][0];
+		metadata['targetHref'] = categoryToDo[0][1];
+	} else {
+		for(let i = 0; i < categoryToDo.length; i++) {
+			if(categoryToDo[i][1] == targetHref)
+				return;
+		}
+		categoryToDo.push([targetTitle, targetHref]);
+		return;
+	}
 	var sm = browser.storage.local.get(null);
-	sm.then(function(data) {storageManagerStream(data, caller);});
+	sm.then(function(data) {storageManagerStream(data, metadata);})
 }
 
 // check if requested category is already stored
-function storageManagerStream(storedData, caller) {
-	if(targetHref.hostname in storedData) { // if the wiki's category is already stored, return
-		var content = JSON.parse(storedData[targetHref.hostname]);
+function storageManagerStream(storedData, metadata) {
+	if(metadata['targetHref'].hostname in storedData) { // if the wiki's category is already stored, return
+		var content = JSON.parse(storedData[metadata['targetHref'].hostname]);
 		for(let i = 0; i < content.length; i++) {
-			if(content[i]['title'] == targetTitle)
+			if(content[i]['title'] == metadata['targetTitle'])
 				return;
 		}
 	}
-	scrapeCategoryTask(storedData, targetHref, caller);
+	scrapeCategoryTask(storedData, metadata);
 }
 
 // load category document for processing, web request
-function scrapeCategoryTask(storedData, target, caller) {
+function scrapeCategoryTask(storedData, metadata) {
 	var req = new XMLHttpRequest();
-	req.open('GET', target);
+	req.open('GET', metadata['targetHref']);
 	req.responseType = 'document';
 	req.setRequestHeader('Access-Control-Allow-Origin', '*'); // User Agent?
 	req.setRequestHeader('Accept', 'text/html');
 	req.addEventListener('load', function() {
 		if(req.readyState === 4 && req.status === 200) { // TODO status != 200 ?
-			scrapeCategoryList(req.responseXML, storedData, caller);
+			scrapeCategoryList(req.responseXML, storedData, metadata);
 		} else {
-			transmitError(caller, browser.i18n.getMessage("errorWebReqFail"));
-			freeBlockedEvent(caller);
+			transmitError(metadata['caller'], browser.i18n.getMessage("errorWebReqFail"));
+			freeBlockedEvent(metadata['caller']);
 		}
 	});
 	req.addEventListener('error', function() {
-		transmitError(caller, browser.i18n.getMessage("errorWebReqFail"));
-		freeBlockedEvent(caller);
+		transmitError(metadata['caller'], browser.i18n.getMessage("errorWebReqFail"));
+		freeBlockedEvent(metadata['caller']);
 	});
 	req.send();
 }
 
 // search HTML code for category entries
-function scrapeCategoryList(content, storedData, caller) {
+function scrapeCategoryList(content, storedData, metadata) {
 	var catLinks = [];
 	var hasNext = false;
 	if(content.querySelectorAll('#mw-pages .mw-category').length == 1) { // locate category area and grab individual category entries
@@ -131,36 +149,48 @@ function scrapeCategoryList(content, storedData, caller) {
 			hasNext = content.querySelector('a.category-page__pagination-next').href;
 	}
 	var catList = {};
-	var replacer = targetHref.href.replace(targetHref.pathname, '');
+	var replacer = metadata['targetHref'].href.replace(metadata['targetHref'].pathname, '');
 	for(let i = 0; i < catLinks.length; i++) { // fill category list, adjust href format for addon-use
 		catList[catLinks[i].title] = catLinks[i].href.replace(replacer, '');
 	}
 
 	var content;
-	if(targetHref.hostname in storedData) {
-		content = JSON.parse(storedData[targetHref.hostname]);
+	if(metadata['targetHref'].hostname in storedData) {
+		content = JSON.parse(storedData[metadata['targetHref'].hostname]);
 	} else {
 		content = [];
 	}
 	var existing = false;
 	content.forEach(function(c) { // check if category is already stored, add additional items
-		if(c['title'] == targetTitle) {
+		if(c['title'] == metadata['targetTitle']) {
 			c['items'] = {... c['items'], ...catList};
 			existing = true;
 		}
 	});
 	if(!existing)
-		content.push({'title': targetTitle, 'path': targetHref.pathname, 'protocol': targetHref.protocol, 'items' : catList});
-	storedData[targetHref.hostname] = JSON.stringify(content);
+		content.push({'title': metadata['targetTitle'], 'path': metadata['targetHref'].pathname, 'protocol': metadata['targetHref'].protocol, 'items' : catList});
+	storedData[metadata['targetHref'].hostname] = JSON.stringify(content);
 
 	if(hasNext !== false) { // if next-page is available, call next page
-		if(!hasNext.startsWith(targetHref.protocol + '//' + targetHref.hostname))
-			hasNext = targetHref.protocol + '//' + targetHref.hostname + hasNext;
-		scrapeCategoryTask(storedData, new URL(hasNext), caller);
+		if(!hasNext.startsWith(metadata['targetHref'].protocol + '//' + metadata['targetHref'].hostname))
+			hasNext = metadata['targetHref'].protocol + '//' + metadata['targetHref'].hostname + hasNext;
+		scrapeCategoryTask(storedData, {'caller': metadata['caller'], 'targetTitle': metadata['targetTitle'],'targetHref': new URL(hasNext)});
 	} else {
 		browser.storage.local.set(storedData);
-		freeBlockedEvent(caller);
+		checkToDo(metadata);
+		freeBlockedEvent(metadata['caller']);
 	}
+}
+
+function checkToDo(metadata) {
+	for(let i = 0; i < categoryToDo.length; i++) {
+		if(categoryToDo[i] == [metadata['targetTitle'], metadata['targetHref']])
+			categoryToDo.splice(i, 1);
+	}
+	if(categoryToDo.length == 0)
+		lock = false;
+	else
+		storageManager(metadata['caller'], true);
 }
 
 function transmitError(to, errorMsg) {
